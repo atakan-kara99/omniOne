4a8e084c0787f1ba66bf30e4abb24fd27a7753fc
Yilmaz Atakan Kara <stu216493@mail.uni-kiel.de>
2025-12-13 15:12:48 +0100

    added target ignore on mappings

--- src/main/java/app/omniOne/model/mapper/QuestionnaireMapper.java
+++ src/main/java/app/omniOne/model/mapper/QuestionnaireMapper.java
@@ -5,12 +5,16 @@ import app.omniOne.model.dto.QuestionnaireQuestionResponse;
 import app.omniOne.model.entity.questionnaire.QuestionnaireAnswer;
 import app.omniOne.model.entity.questionnaire.QuestionnaireQuestion;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface QuestionnaireMapper {
 
     QuestionnaireQuestionResponse map(QuestionnaireQuestion question);
 
+    @Mapping(target = "questionId", ignore = true)
+    @Mapping(target = "answerText", ignore = true)
+    @Mapping(target = "questionText", ignore = true)
     QuestionnaireAnswerResponse map(QuestionnaireAnswer answer);
 
 }
