3e9320d0257c49be8571bc2ff1e8f4cb58441607
Yilmaz Atakan Kara <stu216493@mail.uni-kiel.de>
2026-01-29 14:01:34 +0100

    included password validation

--- src/main/java/app/omniOne/authentication/model/LoginRequest.java
+++ src/main/java/app/omniOne/authentication/model/LoginRequest.java
@@ -2,6 +2,8 @@ package app.omniOne.authentication.model;
 
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
 
@@ -9,17 +11,17 @@ public record LoginRequest(
         String email,
 
         @NotBlank
-//   @Size(min = 8, max = 64)
-//   @Pattern(regexp = "^\\S+$", message = "Password cannot contain spaces")
-//   @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d])[A-Za-z\\d\\W_]{8,64}$",
-//           message = "Password must contain upper, lower, digit, and special character.")
+        @Size(min = 8, max = 32)
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)\\S{8,32}$",
+                message = "Password must be 8–32 characters and include at least one uppercase letter, " +
+                        "one lowercase letter, and one number. Special characters are allowed. No spaces.")
         String password
 
 ) {
 
         @Override
         public @Nonnull String toString() {
-                return "LoginRequest[username=" + username + ", password=***]";
+                return "LoginRequest[email=" + email + ", password=***]";
         }
 
 }
